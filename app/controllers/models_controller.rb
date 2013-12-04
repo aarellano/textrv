2c6e45c060aaaa2cdf5959e8d18c1a74227c1d55
Andres Arellano <andres.arellano@gmail.com>
2013-12-03 20:29:00 -0500

    Fixes a typo

--- app/controllers/models_controller.rb
+++ app/controllers/models_controller.rb
@@ -64,7 +64,7 @@ class ModelsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_model
-      @model = model.find(params[:id])
+      @model = Model.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
