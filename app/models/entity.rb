9a81a28b30fb07d48c712e82cc7535f7393817cb
Andres Arellano <andres.arellano@gmail.com>
2013-12-12 14:21:18 -0500

    Adds names to array method to properties

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -2,4 +2,13 @@ class Entity < ActiveRecord::Base
   belongs_to :model
   has_many :properties
   has_many :systems
+
+  def properties_array
+    array = []
+    self.properties.each do |prop|
+      array.push prop.name
+    end
+    return array
+  end
+
 end
